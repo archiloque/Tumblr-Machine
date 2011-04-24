2870a703b122f35c55bab5e764fd2bef825cf542
Julien Kirch <code@archiloque.net>
2011-04-24 11:12:34 +0200

    can now insert tags

--- tumblr-machine.rb
+++ tumblr-machine.rb
@@ -10,6 +10,8 @@ require 'sinatra/base'
 require 'sinatra'
 require 'sinatra/sequel'
 
+require 'rack-flash'
+
 Sequel::Model.raise_on_save_failure = true
 require 'erb'
 
@@ -31,6 +33,10 @@ class TumblrMachine< Sinatra::Base
     database.loggers << Logger.new(STDOUT)
   end
 
+  use Rack::Session::Pool
+  use Rack::Flash
+
+  require_relative 'lib/helpers'
 
   #migrations
   migration 'create tables' do
@@ -38,7 +44,7 @@ class TumblrMachine< Sinatra::Base
       primary_key :id, :type => Integer, :null => false
       Text :name, :null => false, :index => true, :unique => true
       DateTime :last_fetch_date, :null => true
-      Integer :score, :null => true
+      Integer :value, :null => true
     end
 
     database.create_table :tumblrs do
@@ -47,7 +53,7 @@ class TumblrMachine< Sinatra::Base
       DateTime :last_post_date, :null => true
     end
 
-    database.create_table :post do
+    database.create_table :posts do
       primary_key :id, :type => Integer, :null => false
       foreign_key :tumblr_id, :tumblrs
       DateTime :last_fetch_date, :null => false
@@ -66,7 +72,7 @@ class TumblrMachine< Sinatra::Base
   end
 
   #models
-  class Tubmlr < Sequel::Model
+  class Tumblr < Sequel::Model
     one_to_many :posts
   end
 
@@ -75,8 +81,40 @@ class TumblrMachine< Sinatra::Base
     many_to_one :tumblr
   end
 
+  # admin
   get '/' do
+    erb :'admin.html'
+  end
 
+  post '/add_tag' do
+    name = params[:tagName]
+    value = params[:tagValue]
+    if name.blank?
+      flash[:error] = 'Tag name is empty'
+      redirect '/'
+    elsif value.blank?
+      flash[:error] = 'Tag value is empty'
+      redirect '/'
+    else
+      value = value.to_i
+      if value == 0
+        value = nil
+      end
+
+      if tag = Tag.first(:name => name)
+        tag.value = value
+        tag.insert
+        flash[:notice] = 'Tag updated'
+      else
+        if value
+          Tag.create(:name => name, :value => value)
+          flash[:notice] = 'Tag added'
+        else
+          flash[:notice] = 'Tag did not exist and value is 0 so nothing done'
+        end
+      end
+      redirect '/'
+    end
   end
 
 end
