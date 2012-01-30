40a9194d89c351758ea9c95649c1f5d91e7f5ee5
Julien Kirch <jkirch@plizy.com>
2012-01-30 22:20:44 +0100

    Include migration in table creation

--- lib/models.rb
+++ lib/models.rb
@@ -4,26 +4,30 @@ require 'phashion'
 #migrations
 migration 'create tables' do
   database.create_table :tags do
-    primary_key :id, :type => Integer, :null => false
+    primary_key :id, :type => Bignum, :null => false
     Text :name, :null => false, :index => true, :unique => true
     DateTime :last_fetch, :null => true
     Boolean :fetch, :null => false, :default => false, :index => true
-    Integer :value, :null => true
+    Integer :value, :null => false
   end
 
   database.create_table :tumblrs do
-    primary_key :id, :type => Integer, :null => false
-    Text :name, :null => false, :index => true, :unique => true
+    primary_key :id, :type => Bignum, :null => false
+    Text :name, :null => false, :index => true
     Text :url, :null => false, :index => true, :unique => true
     DateTime :last_reblogged_post, :null => true
   end
 
   database.create_table :posts do
-    primary_key :id, :type => Integer, :null => false
+    primary_key :id, :type => Bignum, :null => false
     foreign_key :tumblr_id, :tumblrs
-    DateTime :fetched, :null => false
+    DateTime :fetched, :null => false, :index => true
     Boolean :posted, :null => true, :index => true, :default => false
-    Integer :score, :null => true, :index => true
+    Integer :score, :null => false, :index => true
+    Boolean :skip, :null => true, :index => true
+    Text :img_url, :null => true
+    Integer :height, :null => true
+    Integer :width, :null => true
   end
 
   database.create_table :posts_tags do
@@ -32,56 +36,6 @@ migration 'create tables' do
   end
 end
 
-migration 'larger columns for posts ids' do
-  database.alter_table :posts do
-    set_column_type :id, Bignum
-  end
-  database.alter_table :posts_tags do
-    set_column_type :post_id, Bignum
-  end
-end
-
-migration 'no null tags score' do
-  database.run 'update tags set value = 0 where value is null'
-  database.alter_table :tags do
-    set_column_allow_null :value, false
-  end
-  database.run 'update posts set score = 0 where score is null'
-  database.alter_table :posts do
-    set_column_allow_null :score, false
-  end
-end
-
-migration 'skipping posts' do
-  database.alter_table :posts do
-    add_column :skip, :boolean, :null => true, :index => true
-  end
-end
-
-migration 'posts img' do
-  database.alter_table :posts do
-    add_column :img_url, :text, :null => true
-  end
-end
-
-migration 'fetched index' do
-  database.alter_table :posts do
-    add_index :fetched, :unique => false
-  end
-end
-
-migration 'images sizes' do
-  database.alter_table :posts do
-    add_column :height, :integer, :null => true
-    add_column :width, :integer, :null => true
-  end
-end
-
-migration 'tumblr name can change 2' do
-  database.alter_table :tumblrs do
-    drop_index [:name], :name => 'tumblrs_name_key'
-  end
-end
 
 if DEDUPLICATION
 
