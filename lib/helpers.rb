4d48a862eabe7b60fe7ad73fa18ab0bcd675b4ac
Julien Kirch <jkirch@octo.com>
2013-07-22 19:51:47 +0200

    Link to local image inly when available

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,11 +5,18 @@ module Sinatra
       Rack::Utils.escape_html(text)
     end
 
-    def display_date_time date, between = ''
+    def display_date_time(date, between = '')
       if date
         date.strftime("%d/%m/%Y #{between}%H:%M:%S")
       end
     end
+
+    def img_src(post)
+      if post.img_saved && File.exist?(File.join(STORED_IMAGES_DIR, "#{post.id}#{File.extname(post.img_url)}"))
+          "/stored_images/#{post.id}#{File.extname(post.img_url)}"
+      else
+        post.img_url
+      end
 
   end
 end
